19d08336facdc331198bf35218dcbf5a291ccd42
stefarenina <121181192+stefarenina@users.noreply.github.com>
2023-10-31 15:30:53 -0600

    paneldeAdministracion v2

--- JS/controllers/admins.js
+++ JS/controllers/admins.js
@@ -8,6 +8,8 @@ const botonesEliminar = document.querySelectorAll(".eliminarBoton");
 const confirmarEliminacion = document.getElementById("afirmativoEliminar");
 const recharazarEliminacion =  document.getElementById("negativoEliminar");
 
+const reporteBoton = document.getElementById("reporteBoton");
+
 botonesEliminar.forEach(function(boton) {
     boton.addEventListener('click', function() {
         ventana.showModal();
@@ -21,7 +23,7 @@ confirmarEliminacion.addEventListener("click", () => {
     swal.fire({
         icon: 'success',
         title: 'Success',
-        text: 'Se ha eliminado el usuario'
+        text: 'Se ha eliminado este dato'
     });    
 });
 
@@ -29,3 +31,10 @@ recharazarEliminacion.addEventListener("click", () => {
     ventana.close();   
 });
 
+reporteBoton.addEventListener("click", () => {
+    swal.fire({
+        icon: 'success',
+        title: 'Success',
+        text: 'Se ha generado un reporte'
+    }); 
+});
